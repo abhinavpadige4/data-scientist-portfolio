59ace2e3c0c1e60328bbbc987ec252c9b5415eb7
abhinav <abhinavpadige06@gmail.com>
2026-09-10 23:47:06 +0530

    feat: add logo component

---
+++ components/layout/Logo.tsx
@@ -0,0 +1,14 @@
+export default function Logo({ className }: { className?: string }) {
+  return (
+    <svg 
+      width="24" 
+      height="24" 
+      viewBox="0 0 24 24" 
+      fill="none" 
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+}
